80bc894c36e30f7bbee5e7b66aad92f3bf29eff6
adpolueva <git@MacBook-Pro-Vladislav.local>
2020-11-24 18:36:25 +0300

    lab 1

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+class A{
+    
+}
+
+class B extends A
+{
+    public function __construct($a)
+    {
+        $this->a = $a;
+        
+    }
+    
+    protected $a;  
+}
+
+
+class C extends B
+{
+    public function __construct($a,$b,$c)
+    {
+		$this->b = $b;
+        $this->c = $c;
+        parent::__construct($a);
+    }
+	protected $b;
+    protected $c;
+    
+}
+
+$a1 = new A ();
+$a2 = new A ();
+$a3 = new A ();
+$b4 = new B ($a3);
+$c5 = new C ($b4,$a1,$a2);
+
+?>
